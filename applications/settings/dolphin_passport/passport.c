d1351e581b89fee51b7b9300324efb1f15159b5f
AlphaxHax <49750262+AlphaxHax@users.noreply.github.com>
2022-12-30 10:12:32 +0000

    Slight Changes before release

--- applications/settings/dolphin_passport/passport.c
+++ applications/settings/dolphin_passport/passport.c
@@ -43,13 +43,13 @@ static void render_callback(Canvas* canvas, void* ctx) {
 
     if(stats->butthurt <= 4) {
         mood = 0;
-        snprintf(mood_str, 20, "Status: Naughty");
+        snprintf(mood_str, 20, "Mood: Extatic");
     } else if(stats->butthurt <= 9) {
         mood = 1;
-        snprintf(mood_str, 20, "Status: Hyper");
+        snprintf(mood_str, 20, "Mood: Chaotic");
     } else {
         mood = 2;
-        snprintf(mood_str, 20, "Status: grumpy");
+        snprintf(mood_str, 20, "Mood: Fuming");
     }
 
     uint32_t xp_progress = 0;
